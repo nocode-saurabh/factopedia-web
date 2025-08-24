813aa28583769594cc12285ffeffb7fd62b0c58b
nocode-saurabh <iamsaurabh.maske@gmail.com>
2025-08-24 12:49:56 -0400

    updated config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const config = {
-    API_URL: process.env.NODE_ENV === 'production' 
-      ? 'https://web-scrapper-gv8s.onrender.com/'  
-      : 'http://localhost:4000'
+    API_URL: process.env.REACT_APP_API_URL || 'http://localhost:4000'
   };
   
   export default config;
