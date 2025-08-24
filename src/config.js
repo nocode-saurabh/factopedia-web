8f2db3497d7d57a88ba11370584775305596a84e
nocode-saurabh <iamsaurabh.maske@gmail.com>
2025-08-24 12:38:51 -0400

    updated backend base url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const config = {
-    // Use environment variables or different URLs for development/production
     API_URL: process.env.NODE_ENV === 'production' 
-      ? 'https://your-backend-url.com'  // Replace with your actual backend URL
+      ? 'https://web-scrapper-gv8s.onrender.com/'  
       : 'http://localhost:4000'
   };
   
