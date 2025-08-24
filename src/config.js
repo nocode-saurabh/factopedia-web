cf2e1d56aae21e1e48f4a80252852207bb40842b
nocode-saurabh <iamsaurabh.maske@gmail.com>
2025-08-24 13:18:46 -0400

    update env var

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:4000'
+  API_URL: process.env.NODE_ENV === 'production' 
+    ? process.env.REACT_APP_API_URL || 'https://web-scrapper-gv8s.onrender.com/'
+    : 'http://localhost:4000'
 };
 
 // Debug information
-console.log('Environment:', process.env.NODE_ENV);
+console.log('Build Environment:', process.env.NODE_ENV);
 console.log('API URL from env:', process.env.REACT_APP_API_URL);
 console.log('Final API URL:', config.API_URL);
 
